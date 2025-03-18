06c6270efd3b9f1d5582820243308e502daa6127
Rushikesh Sonawane <38025576+code-rushi@users.noreply.github.com>
2025-03-18 17:48:40 +0000

    Conversions of datatypes

---
+++ codes/03.Datatype_conversions.js
@@ -0,0 +1,23 @@
+let score = "33ab"
+
+//console.log(score);
+let UserPassword = Number (score);
+//console.log(UserPassword);
+//console.log(typeof(UserPassword));
+//console.log(typeof (score))
+
+let BooleanValue  = 1
+console.log(BooleanValue)
+
+
+let convetedValue = Boolean (BooleanValue);
+console.log(convetedValue);
+
+console.log(typeof(BooleanValue))//Number
+console.log(typeof(convetedValue)) // Boolean 
+//Converted Boolean Value 
+
+//1 = true
+//0= false
+//Rushi = True
+
