cf13e42e9e5de125bf12da57f26154bcf50f4166
Rushikesh Sonawane <38025576+code-rushi@users.noreply.github.com>
2025-03-18 04:48:40 +0000

    datatypes and variables

---
+++ codes/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js  code as newer version 
+
+//alert(3+3)// it  will not work here becuase we are using node.js and not  a browser
+
+// Datatype 
+
+//number 
+//bigInt 
+//string => "doble quote", 'single quote'
+//boolen=> True/False
+//null => standalone  value
+//undefined =>
+// symbol =>
+
+    console.log(typeof null);
+    console.log(typeof undefined)
+    
+    let name="Rushi"
+    let age =  25.5
+    let rich = true
+
+console.log(typeof name);
+console.log(typeof age)
+console.log(typeof rich)
---
+++ codes/variables.js
@@ -0,0 +1,8 @@
+const userId = 1542
+let accountEmail= "rushi4025@gmail.com" 
+var accountPw = "Rishi123"
+accountCity="Pune"
+
+//userId = 253
+
+console.log(accountCity);
